dce4ef8a41e941a336c2028cd3b400964ff1b1b8
dpmoorthy <32807542+dpmoorthy@users.noreply.github.com>
2024-12-28 22:29:58 +0530

    updated

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as s3 from 'aws-cdk-lib/aws-s3'
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -12,5 +12,11 @@ export class InfraStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'InfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    //S3 Bucket
+     const s3demobucket = new s3.Bucket(this, 's3demobucket2024' ,{
+      bucketName: 'demos3bucket202228',
+      versioned: true,
+      publicReadAccess: false
+     })
   }
 }
